8532a30ef367de238e8face869f54256480e7a74
golly10 <juanjo96mt@gmail.com>
2022-04-09 15:52:51 +0200

    REady to write

--- Tema 4/Actividad2/generate_contrast_set.py
+++ Tema 4/Actividad2/generate_contrast_set.py
@@ -2,7 +2,13 @@ from scipy.io import arff
 import pandas as pd
 
 
-def get_patterns(data):
+def get_patterns(path, len_data):
+
+    data = open(path, "r")
+
+    data = data.read()
+
+    data = data.split("\n")
 
     patterns = []
 
@@ -16,11 +22,13 @@ def get_patterns(data):
             ptt.split(":")[0]
             .replace(ptt.split(":")[1].strip(), "")
             .strip()
+            .split("==>")[0]
+            .strip()
         )
 
         patterns.append(rule)
 
-        supports.append(int(ptt.split(":")[1].strip()) / len(data))
+        supports.append(int(ptt.split(":")[1].strip()) / len_data)
 
     data_df["patterns"] = patterns
 
@@ -29,48 +37,151 @@ def get_patterns(data):
     return data_df
 
 
+def get_supp_diff(patt_an, other_dts):
+
+    # Inicializamos las columnas necesarias
+    for dt in range(1, len(other_dts) + 1):
+        patt_an["supp_" + str(dt)] = 0.0
+        patt_an["diff_" + str(dt)] = 0.0
+
+    patt_an["max_diff"] = 0.0
+
+    # Calculamos el soporte en cada uno de los otros subconjuntos
+    for index, row in patt_an.iterrows():
+
+        i = 1
+        for dts in other_dts:
+
+            if row["patterns"] in list(dts["patterns"]):
+
+                patt_an["supp_" + str(i)][index] = dts[
+                    dts["patterns"] == row["patterns"]
+                ]["supports"]
+
+            i += 1
+
+    for index, row in patt_an.iterrows():
+
+        # Calculamos todas las diferencias con los soportes ya calculados
+        for dt in range(1, len(other_dts) + 1):
+            patt_an["diff_" + str(dt)][index] = abs(
+                patt_an["supports"][index] - patt_an["supp_" + str(dt)][index]
+            )
+
+        # Calculamos el maximo de todas las diferencias
+        for dt in range(1, len(other_dts) + 1):
+
+            if patt_an["max_diff"][index] < patt_an["diff_" + str(dt)][index]:
+                patt_an["max_diff"][index] = patt_an["diff_" + str(dt)][index]
+
+    return patt_an.sort_values("max_diff", ascending=False)
+
+
 if __name__ == "__main__":
 
     # Leemos los patrones encontrados tras aplicar el algoritmo Apriori
-    # sobre el subconjunto de datos 1st
-    data_1st = open("Tema 4/Actividad2/dataset/results_data1st.txt", "r")
+    # sobre los subconjuntos de datos
 
-    data_1st = data_1st.read()
+    patterns_data_1st = get_patterns(
+        "Tema 4/Actividad2/dataset/results_data1st.txt", len_data=325
+    )
 
-    data_1st = data_1st.split("\n")
+    patterns_data_2nd = get_patterns(
+        "Tema 4/Actividad2/dataset/results_data2nd.txt", len_data=285
+    )
 
-    patterns_data_1st = get_patterns(data_1st)
+    patterns_data_3rd = get_patterns(
+        "Tema 4/Actividad2/dataset/results_data3rd.txt", len_data=706
+    )
 
-    # Leemos los patrones encontrados tras aplicar el algoritmo Apriori
-    # sobre el subconjunto de datos 2nd
-    data_2nd = open("Tema 4/Actividad2/dataset/results_data2nd.txt", "r")
+    patterns_data_crew = get_patterns(
+        "Tema 4/Actividad2/dataset/results_datacrew.txt", len_data=885
+    )
 
-    data_2nd = data_2nd.read()
+    # Calculamos los soportes de las reglas de 1st sobre los demas conjuntos,
+    # su diferencia entre soportes y el máximo de esas diferencias
+    support_patt_1st = get_supp_diff(
+        patterns_data_1st, [patterns_data_2nd, patterns_data_3rd, patterns_data_crew]
+    )
 
-    data_2nd = data_2nd.split("\n")
+    support_patt_1st.columns = [
+        "patterns",
+        "supp_1st",
+        "supp_2nd",
+        "diff_2nd",
+        "supp_3rd",
+        "diff_3rd",
+        "supp_crew",
+        "diff_crew",
+        "max_diff",
+    ]
+    support_patt_1st["class"] = "1st"
 
-    patterns_data_2nd = get_patterns(data_2nd)
+    # Calculamos los soportes de las reglas de 2nd sobre los demas conjuntos,
+    # su diferencia entre soportes y el máximo de esas diferencias
+    support_patt_2nd = get_supp_diff(
+        patterns_data_2nd, [patterns_data_1st, patterns_data_3rd, patterns_data_crew]
+    )
 
-    # Leemos los patrones encontrados tras aplicar el algoritmo Apriori
-    # sobre el subconjunto de datos 12rd
-    data_3rd = open("Tema 4/Actividad2/dataset/results_data3rd.txt", "r")
+    support_patt_2nd.columns = [
+        "patterns",
+        "supp_2nd",
+        "supp_1st",
+        "diff_1st",
+        "supp_3rd",
+        "diff_3rd",
+        "supp_crew",
+        "diff_crew",
+        "max_diff",
+    ]
+    support_patt_2nd["class"] = "2nd"
 
-    data_3rd = data_3rd.read()
+    # Calculamos los soportes de las reglas de 3rd sobre los demas conjuntos,
+    # su diferencia entre soportes y el máximo de esas diferencias
+    support_patt_3rd = get_supp_diff(
+        patterns_data_3rd, [patterns_data_1st, patterns_data_2nd, patterns_data_crew]
+    )
 
-    data_3rd = data_3rd.split("\n")
+    support_patt_3rd.columns = [
+        "patterns",
+        "supp_3rd",
+        "supp_1st",
+        "diff_1st",
+        "supp_2nd",
+        "diff_2nd",
+        "supp_crew",
+        "diff_crew",
+        "max_diff",
+    ]
+    support_patt_3rd["class"] = "3rd"
 
-    patterns_data_3rd = get_patterns(data_3rd)
+    # Calculamos los soportes de las reglas de crew sobre los demas conjuntos,
+    # su diferencia entre soportes y el máximo de esas diferencias
+    support_patt_crew = get_supp_diff(
+        patterns_data_crew, [patterns_data_1st, patterns_data_2nd, patterns_data_3rd]
+    )
+    support_patt_crew.columns = [
+        "patterns",
+        "supp_crew",
+        "supp_1st",
+        "diff_1st",
+        "supp_2nd",
+        "diff_2nd",
+        "supp_3rd",
+        "diff_3rd",
+        "max_diff",
+    ]
+    support_patt_crew["class"] = "crew"
 
-    # Leemos los patrones encontrados tras aplicar el algoritmo Apriori
-    # sobre el subconjunto de datos crew
-    data_crew = open("Tema 4/Actividad2/dataset/results_datacrew.txt", "r")
+    supports = pd.concat(
+        [
+            pd.concat(
+                [pd.concat([support_patt_1st, support_patt_2nd]), support_patt_3rd]
+            ),
+            support_patt_crew,
+        ],
+    )
 
-    data_crew = data_crew.read()
+    supports = supports.sort_values("max_diff", ascending=False)
 
-    data_crew = data_crew.split("\n")
-
-    patterns_data_crew = get_patterns(data_crew)
-
-    data_all = pd.DataFrame()
-
-
+    supports
